725f96f58566adccc89460127a9591c869891ce7
Rishi-2028 <rishibhandari2028@gmail.com>
2023-03-12 16:25:14 +0545

    Initial Commit

---
+++ Crypto-Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto-Tracker
+//
+//  Created by Rishi on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

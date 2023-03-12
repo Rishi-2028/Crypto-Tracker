b3d087fd4d8225fc62bacd744dc0539d9b6e3870
Rishi-2028 <rishibhandari2028@gmail.com>
2023-03-12 18:47:41 +0545

    Creating HomeView And Custom NavigationHeader

--- Crypto-Tracker/Crypto_TrackerApp.swift
+++ Crypto-Tracker/Crypto_TrackerApp.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct Crypto_TrackerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
+           
         }
     }
 }
---
+++ Crypto-Tracker/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  Crypto-Tracker
+//
+//  Created by Rishi on 12/03/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
